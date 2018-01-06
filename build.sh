84ba8eb1cc092f3d9a95fa57235a1417976122c6
Curdin Caspar <curdin.caspar@gmail.com>
2018-01-06 21:16:56 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-sudo docker build -t centralbackup:0.1 .
-sudo docker tag centralbackup:0.1 curcas/centralbackup
-sudo docker push curcas/centralbackup
+sudo docker build -t curcas/centralbackup:latest .
+sudo docker tag curcas/centralbackup:latest curcas/centralbackup:0.1
+sudo docker push curcas/centralbackup:latest
+sudo docker push curcas/centralbackup:0.1
